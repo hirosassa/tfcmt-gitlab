5735aeba301ed313d5f46d1b692c2a00bb42edbb
Ryo Kitagawa <kitadrum50@gmail.com>
2024-04-11 05:23:59 +0900

    add gomock for test (#22)
    * refactor: add gomock for test
    
    * fix: update aqua-installer to v3.0.0

--- go.mod
+++ go.mod
@@ -6,15 +6,12 @@ require (
 	github.com/Masterminds/sprig/v3 v3.2.2
 	github.com/drone/envsubst v1.0.3
 	github.com/google/go-cmp v0.5.7
-	github.com/google/go-github/v39 v39.2.0
 	github.com/mattn/go-colorable v0.1.12
-	github.com/shurcooL/githubv4 v0.0.0-20220115235240-a14260e6f8a2
 	github.com/sirupsen/logrus v1.8.1
-	github.com/suzuki-shunsuke/github-comment-metadata v0.1.0
-	github.com/suzuki-shunsuke/go-ci-env v1.1.0
 	github.com/suzuki-shunsuke/go-findconfig v1.1.0
 	github.com/urfave/cli/v2 v2.4.0
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+	github.com/xanzy/go-gitlab v0.64.0
+	go.uber.org/mock v0.4.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -35,13 +32,14 @@ require (
 	github.com/mitchellh/reflectwalk v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
-	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
 	github.com/spf13/cast v1.3.1 // indirect
-	github.com/xanzy/go-gitlab v0.64.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- pkg/notifier/gitlab/commits_test.go
+++ pkg/notifier/gitlab/commits_test.go
@@ -2,36 +2,64 @@ package gitlab
 
 import (
 	"testing"
+
+	gitlabmock "github.com/hirosassa/tfcmt-gitlab/pkg/notifier/gitlab/gen"
+	gitlab "github.com/xanzy/go-gitlab"
+	"go.uber.org/mock/gomock"
 )
 
 func TestCommitsList(t *testing.T) {
 	t.Parallel()
 	testCases := []struct {
-		revision string
-		ok       bool
+		name                string
+		createMockGitLabAPI func(ctrl *gomock.Controller) *gitlabmock.MockAPI
+		revision            string
+		ok                  bool
 	}{
 		{
+			name: "should list commits",
+			createMockGitLabAPI: func(ctrl *gomock.Controller) *gitlabmock.MockAPI {
+				api := gitlabmock.NewMockAPI(ctrl)
+				api.EXPECT().ListCommits(gomock.Cond(func(x any) bool {
+					_, ok := x.(*gitlab.ListCommitsOptions)
+					return ok
+				})).Return([]*gitlab.Commit{}, nil, nil)
+				return api
+			},
 			revision: "04e0917e448b662c2b16330fad50e97af16ff27a",
 			ok:       true,
 		},
 		{
+			name: "should return error when revision is empty",
+			createMockGitLabAPI: func(ctrl *gomock.Controller) *gitlabmock.MockAPI {
+				api := gitlabmock.NewMockAPI(ctrl)
+				return api
+			},
 			revision: "",
 			ok:       false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		cfg := newFakeConfig()
-		client, err := NewClient(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		api := newFakeAPI()
-		client.API = &api
-		_, err = client.Commits.List(testCase.revision)
-		if (err == nil) != testCase.ok {
-			t.Errorf("got error %q", err)
-		}
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			cfg := newFakeConfig()
+			client, err := NewClient(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			client.API = testCase.createMockGitLabAPI(mockCtrl)
+
+			_, err = client.Commits.List(testCase.revision)
+			if (err == nil) != testCase.ok {
+				t.Errorf("got error %q", err)
+			}
+		})
 	}
 }
 
@@ -80,8 +108,6 @@ func TestCommitsLastOne(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		api := newFakeAPI()
-		client.API = &api
 		commit, err := client.Commits.lastOne(testCase.commits, testCase.revision)
 		if (err == nil) != testCase.ok {
 			t.Errorf("got error %q", err)
